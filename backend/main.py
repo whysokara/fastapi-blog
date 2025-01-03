538e43c8c87c8d9ab04bb4a8636f869f16d46b60
“whysokara” <kara_himanshu@yahoo.com>
2025-01-03 16:57:17 +0530

    db setup done

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine 
+from db.base_class import Base
 
-## initialising the object
-app = FastAPI(title=settings.PROJECT_TITLE , version=settings.PROJECT_VERSION)
+def create_tables():         
+	Base.metadata.create_all(bind=engine)
+        
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
+
 
 @app.get("/")
-def hello():
-    return {"msg":"Hello World bring it on"}
+def home():
+    return {"msg":"Hello FastAPI🚀"}
+
